850899c1e4f0862149ce9e9369053260de75e95c
pardue95 <cwpardue@gmail.com>
2021-10-12 16:59:03 -0500

    trying to figure it out

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+
+// Array and Object of quiz questions
+var questions = [
+    {
+        quizQuest: "Which built-in method calls a function for each element in the array?",
+        choices: ["while()", "loop()", "forEach()", "None of the above."],
+        answer: "forEach()"
+    },
+    {
+        quizQuest: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        quizQuest: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        quizQuest: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes"
+    },
+    {
+        quizQuest: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    },
+
+];
+
+var score = 0;
+var questionIndex = 0;
