21067423f07ee19b74e66f7e7d7334c3ba279f69
pardue95 <85760640+pardue95@users.noreply.github.com>
2021-10-17 15:48:59 -0500

    high scores are displaying

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
-
-// Array and Object of quiz questions
+//variables
 var score = 0;
 var questionIndex = 0;
 var correctAns = 0;
@@ -16,121 +15,121 @@ var choice2 = document.getElementById("btn2");
 var choice3 = document.getElementById("btn3");
 
 var answerCheck = document.getElementById("answerCheck");
-var listOfHighScores = document.getElementById("listOfHighScores");
+var listHighScores = document.getElementById("listHighScores");
 
 // var linebreak = (document.getElementById("linebreak").style.display = "none");
 var highScores = document.getElementById("highScores").style.display = "none";
 // var quizQuestion = document.getElementById("quiz-question").style.display = "none";
 
 function unhideQuiz() {
-	var x = document.getElementById("wrapper");
-	document.getElementById("quiz-question").style.display = "block";
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	}
+    var x = document.getElementById("wrapper");
+    document.getElementById("quiz-question").style.display = "block";
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
 function hideQuiz() {
     var x = document.getElementById("wrapper");
-	document.getElementById("quiz-question").style.display = "none";
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
+    document.getElementById("quiz-question").style.display = "none";
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 }
 
 function unhideHighScores() {
-	var x = document.getElementById("wrapper");
-	document.getElementById("highScores").style.display = "block";
-	if (x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	};
+    var x = document.getElementById("wrapper");
+    document.getElementById("highScores").style.display = "block";
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    };
 };
 
 function hideHighScores() {
-	var x = document.getElementById("highScores");
-	document.getElementById("highScores").style.display = "none";
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} else {
-		x.style.display = "block";
-	}
+    var x = document.getElementById("wrapper");
+    document.getElementById("highScores").style.display = "none";
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
 };
 
 
 function nextQuestion() {
-	questionTitle.textContent = questions[questionIndex].question;
-	choice0.textContent = questions[questionIndex].choices[0];
-	choice1.textContent = questions[questionIndex].choices[1];
-	choice2.textContent = questions[questionIndex].choices[2];
-	choice3.textContent = questions[questionIndex].choices[3];
+    questionTitle.textContent = questions[questionIndex].question;
+    choice0.textContent = questions[questionIndex].choices[0];
+    choice1.textContent = questions[questionIndex].choices[1];
+    choice2.textContent = questions[questionIndex].choices[2];
+    choice3.textContent = questions[questionIndex].choices[3];
 }
 // check answer
 
 function checkAnswer(answer) {
-	var linebreak = document.getElementById("linebreak");
+    var linebreak = document.getElementById("linebreak");
 
-	linebreak.style.display = "block";
-	answerCheck.style.display = "block";
+    linebreak.style.display = "block";
+    answerCheck.style.display = "block";
 
-	if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
-		// correct answer, add 7 score to final score
-		correctAns += 7;
+    if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
+        // correct answer, add 7 score to final score
+        correctAns += 7;
 
-		answerCheck.textContent = "Correct!";
-	} else {
-		// wrong answer, deduct 3 second from timer
-		time -= 3;
-		correctAns -= 2;
-		timeLeft.textContent = time;
-		answerCheck.textContent = "Wrong!";
-	}
+        answerCheck.textContent = "Correct!";
+    } else {
+        // wrong answer, deduct 3 second from timer
+        time -= 3;
+        correctAns -= 2;
+        timeLeft.textContent = time;
+        answerCheck.textContent = "Wrong!";
+    }
 
-	questionIndex++;
-	// ask next question
-	if (questionIndex < questions.length) {
-		nextQuestion();
-	} else {
-		// if no more question, run game over function
-		gameOver();
-	}
+    questionIndex++;
+    // ask next question
+    if (questionIndex < questions.length) {
+        nextQuestion();
+    } else {
+        // if no more question, run game over function
+        gameOver();
+    }
 }
 // functions to check anwer
 function choose0() {
-	checkAnswer(0);
+    checkAnswer(0);
 }
 
 function choose1() {
-	checkAnswer(1);
+    checkAnswer(1);
 }
 
 function choose2() {
-	checkAnswer(2);
+    checkAnswer(2);
 }
 
 function choose3() {
-	checkAnswer(3);
+    checkAnswer(3);
 }
 
 function gameOver() {
     hideQuiz()
-	console.log(quizQuestion)
-	summary.style.display = "block";
-	// quizQuestion.style.diplay = "none";
-	wrapper.style.display = "none";
-	timeLeft.style.display = "none";
+    console.log(quizQuestion)
+    summary.style.display = "block";
+    // quizQuestion.style.diplay = "none";
+    wrapper.style.display = "none";
+    timeLeft.style.display = "none";
 
 
-	// show final score
-	finalScore.textContent = correctAns;
+    // show final score
+    finalScore.textContent = correctAns;
 }
 
-function storeHighScores(event){
+function storeHighScores(event) {
     event.preventDefault();
 
     if (initials.value === "") {
@@ -139,9 +138,9 @@ function storeHighScores(event){
     }
 
     summary.style.display = "none";
-	quizQuestion.style.diplay = "none";
-	wrapper.style.display = "none";
-	timeLeft.style.display = "none";
+    quizQuestion.style.diplay = "none";
+    wrapper.style.display = "none";
+    timeLeft.style.display = "none";
     // highScores.style.display = "block"
     hideQuiz()
     unhideHighScores()
@@ -160,21 +159,21 @@ function storeHighScores(event){
         initials: initials.value,
         score: finalScore.textContent
     };
-console.log(playerScore)
+    console.log(playerScore)
     scoresArr.push(playerScore)
-   
- var scoresArrStr = JSON.stringify(scoresArr);
- localStorage.setItem("high scores", scoresArrStr);
 
- displayHighScores()
+    var scoresArrStr = JSON.stringify(scoresArr);
+    localStorage.setItem("high scores", scoresArrStr);
+
+    displayHighScores()
 
 }
 
 function displayHighScores() {
     summary.style.display = "none";
-	quizQuestion.style.diplay = "none";
-	wrapper.style.display = "none";
-	timeLeft.style.display = "none";
+    quizQuestion.style.diplay = "none";
+    wrapper.style.display = "none";
+    timeLeft.style.display = "none";
     // highScores.style.display = "block"
     hideQuiz()
 
@@ -183,69 +182,68 @@ function displayHighScores() {
     if (savedHighScores === null) {
         return;
     }
-console.log(JSON.parse(savedHighScores))
+    console.log(JSON.parse(savedHighScores))
     var storedHighScores = JSON.parse(savedHighScores)
 
     for (var i = 0; i < storedHighScores.length; i++) {
         var newPlayerScore = document.createElement("p");
         newPlayerScore.innerHTML = storedHighScores[i].initials + ": " + storedHighScores[i].score;
-         };
-		// var highScores = (document.getElementById("highScores").style.display = 'none')
-		//  highScores.style.display = "none"
-	//  hideHighScores()
-		 hideQuiz();
-score = 0;
- questionIndex = 0;
- correctAns = 0;
- time = 75;
-return;
+        listHighScores.appendChild(newPlayerScore);
+    };
+    // var highScores = (document.getElementById("highScores").style.display = 'none')
+    //  highScores.style.display = "none"
+    //  hideHighScores()
+    hideQuiz();
+    score = 0;
+    questionIndex = 0;
+    correctAns = 0;
+    time = 75;
+    return;
 }
 
-submitInitials.addEventListener("click", function(event){ 
+submitInitials.addEventListener("click", function(event) {
     storeHighScores(event);
 });
 
 goBackBtn.addEventListener("click", function() {
-    
+
     wrapper.style.display = "block";
     document.getElementById("highScores").style.display = "none";
 })
 
 function clearHighScores() {
-	// Clear localStorage items 
-	localStorage.clear();
-  }
+    // Clear localStorage items 
+    localStorage.clear();
+}
 
-  clearHighScoresBtn.addEventListener("click",function () {
-	  clearHighScores();
-	  wrapper.style.display = "block";
+clearHighScoresBtn.addEventListener("click", function() {
+    clearHighScores();
+    wrapper.style.display = "block";
     document.getElementById("highScores").style.display = "none";
-  }
-  )
+})
 
 
 ///Function that runs the game
-startGame.addEventListener("click", function () {
+startGame.addEventListener("click", function() {
 
 
-	// startQuizBtn.addEventListener("click", newQuiz);
-	choice0.addEventListener("click", choose0);
-	choice1.addEventListener("click", choose1);
-	choice2.addEventListener("click", choose2);
-	choice3.addEventListener("click", choose3);
+    choice0.addEventListener("click", choose0);
+    choice1.addEventListener("click", choose1);
+    choice2.addEventListener("click", choose2);
+    choice3.addEventListener("click", choose3);
 
-	unhideQuiz();
-	var timer = setInterval(function () {
-		if (time <= 0) {
-			clearInterval(timer);
-			document.getElementById("timer").innerHTML = "GAME OVER!";
-		} else {
-			document.getElementById("timer").innerHTML = time;
-		}
-		time -= 1;
-	}, 1000);
+    unhideQuiz();
+    var timer = setInterval(function() {
+        if (time <= 0) {
+            clearInterval(timer);
+            document.getElementById("timer").innerHTML = "GAME OVER!";
+        } else {
+            document.getElementById("timer").innerHTML = time;
+        }
+        time -= 1;
+    }, 1000);
 
-	nextQuestion();
+    nextQuestion();
 
 
 });
@@ -253,28 +251,28 @@ startGame.addEventListener("click", function () {
 
 ////Question bank
 var questions = [{
-		question: "Which built-in method calls a function for each element in the array?",
-		choices: ["while()", "loop()", "forEach()", "None of the above."],
-		answer: "forEach()",
-	},
-	{
-		question: "The condition in an if / else statement is enclosed within ____.",
-		choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-		answer: "parentheses",
-	},
-	{
-		question: "Arrays in Javascript can be used to store ____.",
-		choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-		answer: "all of the above",
-	},
-	{
-		question: "String values must be enclosed within ____ when being assigned to variables.",
-		choices: ["commas", "curly brackets", "quotes", "parentheses"],
-		answer: "quotes",
-	},
-	{
-		question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-		choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-		answer: "console log",
-	},
+        question: "Which built-in method calls a function for each element in the array?",
+        choices: ["while()", "loop()", "forEach()", "None of the above."],
+        answer: "forEach()",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses",
+    },
+    {
+        question: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "quotes",
+    },
+    {
+        question: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log",
+    },
 ];
